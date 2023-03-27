20210f7b0c7ec22d8a4d0cee110f281c1b069279
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-03-27 17:19:59 -0300

    Exercício-17.py

---
+++ Exercício-17.py
@@ -0,0 +1,9 @@
+### Receba 4 valores de notas e calcule a média aritimética ###
+
+
+n1 = float(input('Nota 1:  '))
+n2 = float(input('Nota 2:  '))
+n3 = float(input('Nota 3:  '))
+n4 = float(input('Nota 4:  '))
+nf = ((n1 + n2 + n3 + n4) / 4)
+print (f'A média aritimética é:  {nf}')
