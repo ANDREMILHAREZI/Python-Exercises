28d496f3d18f185ddc35b3e22a7ce6136de9c480
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-03-28 15:27:57 -0300

    Exercício-22.py

---
+++ Exercício-22.py
@@ -0,0 +1,6 @@
+### Leia o valor do raio de um circulo e calcule e imprima a área do círculo correspondente ###
+import math
+print("Digite o valor do raio de um circulo")
+rai = int(input())
+area = math.pi * rai ** 2
+print(f"O valor da área é: {area}")
