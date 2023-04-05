7fb08cd6a8e7ad2e418c590e23b731139f904976
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-04-05 00:40:45 -0300

    Exercício-25.py

---
+++ Exercício-25.py
@@ -0,0 +1,13 @@
+#### A importância de r$780.000 será dividida entre três ganhadores  de um concurso.####
+####  Sendo que da quantia total total: ####
+#### O primeiro ganhador receberá 46%, ####
+#### O segundo receberá 32 %, ####
+# O terceiro receberá o restante, #
+# Calcule e imprima a quantia ganha por cada um dos ganhadores #
+
+p = int(input('Prêmio do concurso:  '))
+g = p * 0.46
+s = p * 0.32
+t = p * 0.22
+
+print (f'O ganhador receberá R${g} , o segundo colocado receberá R${s} , o terceiro colocado receberá R${t}')
