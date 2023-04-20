5406c715d38a5b597aac7cc8a016401b70d612b1
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-04-19 21:28:48 -0300

    Exercício-31.py

--- Exercício-31.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Digite o número: '))
-while num < 1000 or num > 9999:
-  print ('Digite um num valido')
-  num = int(input())
-digito = str(num)
-print (digito[0])
-print (digito[1])
-print (digito[2])
-print (digito[3])
