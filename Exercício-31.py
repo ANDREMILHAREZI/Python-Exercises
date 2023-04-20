508197341ad263d9aa788fc0e48b6765630193f6
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-04-20 01:00:31 -0300

    Create Exercício-31.py

---
+++ Exercício-31.py
@@ -0,0 +1,7 @@
+seg = int(input('Digite os segundos:  '))
+min =  seg/60
+hor =  seg/3600
+
+print (f'O valor em segundos é {seg}')
+print (f'O valor em minutos  é {min}')
+print (f'O valor em horas  é {hor}')
