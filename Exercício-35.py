b18d2e29211fe8543d779feaeb5438e669b6dabb
André Milharezi <117467177+ANDREMILHAREZI@users.noreply.github.com>
2023-04-26 00:02:01 -0300

    Create Exercício-35.py

---
+++ Exercício-35.py
@@ -0,0 +1,16 @@
+#Achar as medidas do terreno#
+compr = float(input('Qual o comprimento: '))
+larg = float(input('Qual a largura: '))
+#Achar o preço do metro#
+preco =float(input('Qual o preço do metro de tela: '))
+#Área do terreno#
+area = compr * larg
+#Multiplicação pelo preço da tela#
+custo = area * preco
+
+
+print(custo)
+
+
+
+
